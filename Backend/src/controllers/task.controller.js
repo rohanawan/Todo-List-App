d8d6262a83a4f556b10f764f7ab4de6d97784446
rohanawan <rohan.awan863@gmail.com>
2023-06-13 19:17:28 +0500

    Subject: Removed console

--- Backend/src/controllers/task.controller.js
+++ Backend/src/controllers/task.controller.js
@@ -13,7 +13,6 @@ const getTasks = catchAsync(async (req, res) => {
 });
 
 const updateTask = catchAsync(async (req, res) => {
-  console.log(req.params.id)
   const result = await taskService.updateTaskById(req.params.id, req.body);
   res.send(result);
 });
